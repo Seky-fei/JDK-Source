9677006576e39a25a51d99d6c06aa013f7ca892b
wangfei03 <Seky6050>
2020-09-14 15:01:46 +0800

    init project

---
+++ test/src/main/java/com/seky/jdk/TestMain.java
@@ -0,0 +1,12 @@
+package com.seky.jdk;
+
+/**
+ * @author: wangfei
+ * @create: 2020/9/14 14:42
+ * @description:
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        
+    }
+}
